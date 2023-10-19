7ae77f3316b51c8a67441ed6c5e44a8f74f1a913
HerraHabibi <javierre13@hotmail.com>
2023-10-19 20:00:09 +0200

    17 Condicional if

---
+++ js/17. condicional if.js
@@ -0,0 +1,34 @@
+// --- CONDICIONAL IF ---
+
+let dineroDisponible = 4000;
+let dineroRetirar = 200;
+
+// Definimos la condición
+if (dineroDisponible > dineroRetirar) {
+  //Si se cumple la condición se ejecutará esto
+  console.log('Puedes retirar el dinero solicitado');
+} else {
+  //Si no se cumple se ejecutará esto otro
+  console.log('No puedes retirar el dinero solicitado');
+}
+
+dineroDisponible = 4000;
+dineroRetirar = 7000;
+
+// Definimos la condición
+if (dineroDisponible > dineroRetirar) {
+  //Si se cumple la condición se ejecutará esto
+  console.log('Puedes retirar el dinero solicitado');
+} else {
+  //Si no se cumple se ejecutará esto otro
+  console.log('No puedes retirar el dinero solicitado');
+}
+
+// Operadores de comparación:
+/*
+  > Mayor que
+  < Menor que
+  == Igual que
+  >= Mayor o igual que
+  <= Menor o igual que
+*/
