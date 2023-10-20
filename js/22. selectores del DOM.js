bbd519514773b2c816c67ebb12bd8e265415a9bb
HerraHabibi <javierre13@hotmail.com>
2023-10-20 20:19:41 +0200

    22 Selectores del DOM: querySelectorAll

--- js/22. selectores del DOM.js
+++ js/22. selectores del DOM.js
@@ -4,6 +4,10 @@
 
   //Sirve para obtener el objeto con todas las clases que almacena UN SOLO ELEMENTO CONCRETO HTML
 
+console.log('-------------');
+console.log('querySelector');
+console.log('-------------');
+
 //Funciona como en CSS:
   //- para seleccionar una etiqueta - ej.: 'h1'
   //- para seleccionar una clase    - ej.: '.clase'
@@ -33,3 +37,22 @@ console.log(idHeading.id);
 //Solo se obtiene un único elemento
 const enlaces = document.querySelector('.navegacion a');    //Tenemos varios enlaces en la clase .navegacion
 console.log(enlaces);         //Pero sin embargo solo obtenemos información del primer elemento que coincide
+
+
+
+
+// -- querySelectorAll --
+
+  //Funciona como el querySelector, pero con este obtienes un Array con todos los objetos de los elementos que coincidan
+
+  console.log('');
+  console.log('');
+  console.log('-------------');
+  console.log('querySelectorAll');
+  console.log('-------------');
+
+  const enlacesAll = document.querySelectorAll('.navegacion a');    //Tenemos varios enlaces en la clase .navegacion
+  console.log(enlacesAll);      //Y a diferencia del querySelector, obtenemos todos los elementos
+
+  //Pero a diferencia del querySelector, no podemos acceder a los métodos de forma directa, habría que usar un forEach
+  console.log(enlacesAll.tagName);
